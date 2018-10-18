2d592b627c83359a3b7cdb71681db9f3ab5750aa
Miguel Aguilera <miguel.aguilera.croasdaile@gmail.com>
2018-10-18 14:45:09 -0400

    Proceeding to checkout generates an order and order items that reference products; must add method to calculate total cost and quantity of products, but we're getting there

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,5 @@
 class OrdersController < ApplicationController
   def new
-    #Initialize total cost variable
-    @total_cost = 0
-
-    #Iterate over all items in cart to calculate total cost
-    @items = current_user.cart.cart_items
-    @items.each do |item|
-      @product = Product.find(item.product_id)
-      @total_cost += @product.price.to_i
-    end
-
-    #Declare new order
     @order = Order.new
   end
 
@@ -18,6 +7,13 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.user = current_user
     @order.save!
+
+    @cart_items = current_user.cart.cart_items
+    @cart_items.each do |item|
+      @product = Product.find(item.product_id)
+      @order_item = OrderItem.new(order: @order, product: @product)
+      @order_item.save!
+    end
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_033120) do
+ActiveRecord::Schema.define(version: 2018_10_18_181641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2018_10_18_033120) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -63,5 +72,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_033120) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
 end
